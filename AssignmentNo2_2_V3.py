34f44bb0ea9b58e08f85e7ea1d0189e37aeff2a5
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-07 07:14:18 +0800

    Defined a function and added a variable for user to input the number of apples

---
+++ AssignmentNo2_2_V3.py
@@ -0,0 +1,7 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display the output in the following format.
+# The total amount is ______.
+
+def ask_apples_and_oranges_number_to_compute_price():
+    apple_number = int(input("How many apples do you want to buy? "))
